ed5be3b10656892e9eb7dc3a5a909e21fab1bcb6
Urbenson Laurent <ulaurent12@gmail.com>
2018-07-24 23:16:01 +0000

    algo classathon fcc

---
+++ test1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <cs50.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){
+    if (argc != 2){
+        printf("error");
+    }
+
+    int n = atoi(argv[1]);
+    int factor = 1;
+
+
+    for (int i=1; i <= n; i++){
+        factor = factor * i;
+    }
+
+    long long  fix = (long long) factor;
+
+
+   printf ("factorial: %lli\n", fix);
+
+}
---
+++ test2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <cs50.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){
+    if (argc != 2){
+        printf("error");
+    }
+
+    int num = atoi(argv[1]);
+    int first = 0;
+    int second = 1;
+    int result = 0;
+
+    while (second <=  num){
+        if (second % 2 != 0) {
+           result += second;
+         }
+
+         second += first;
+         first = second - first;
+    }
+
+    printf("result: %i", result);
+
+}
